d4e2b120b0beb46bbb0ac281987dcd33ecfa2189
VIRANGA BANDARA <virangabandara97@gmail.com>
2023-02-22 19:37:44 +0530

    GetMapping

--- src/main/java/com/example/crud_springboot/controller/UserController.java
+++ src/main/java/com/example/crud_springboot/controller/UserController.java
@@ -6,16 +6,20 @@ import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
     @Autowired
     private UserService userService;
-    @GetMapping("/getUser")
-    public String getUser(){
-        return "virnga u can";
+
+    @GetMapping("/getUsers")
+    public List<UserDTO> getUser() {
+        return userService.getAllUsers();
     }
+
     @PostMapping("/saveUser")
     public UserDTO saveUser(@RequestBody UserDTO userDTO) {
         return userService.saveUser(userDTO);
