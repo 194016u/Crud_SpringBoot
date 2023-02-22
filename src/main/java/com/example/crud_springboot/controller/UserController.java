d0fef91b3e603c1425f727e130c3c7d1dd657eee
VIRANGA BANDARA <virangabandara97@gmail.com>
2023-02-22 18:52:04 +0530

    Post mapping

--- src/main/java/com/example/crud_springboot/controller/UserController.java
+++ src/main/java/com/example/crud_springboot/controller/UserController.java
@@ -1,19 +1,24 @@
 package com.example.crud_springboot.controller;
 
+import com.example.crud_springboot.dto.UserDTO;
+import com.example.crud_springboot.service.UserService;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/user")
 @CrossOrigin
 public class UserController {
+    @Autowired
+    private UserService userService;
     @GetMapping("/getUser")
     public String getUser(){
         return "virnga u can";
     }
     @PostMapping("/saveUser")
-    public String saveUser(){
-        return "saveUser";
+    public UserDTO saveUser(@RequestBody UserDTO userDTO) {
+        return userService.saveUser(userDTO);
     }
     @PutMapping("/updateUser")
     public String updateUser(){
